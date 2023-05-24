d597d2d408c39639caa30fb714ab408d3cbd8aa3
Elvinadwi24 <126173026+Elvinadwi24@users.noreply.github.com>
2023-05-24 18:04:11 +0700

    method_prosedure.js

---
+++ method_prosedure.js
@@ -0,0 +1,24 @@
+//methode prosedure
+
+function hello(nama , asal, suku) {
+    console.log(
+        'hai semua perkenalkan saya '+
+           nama + 
+         ' dari '+ asal + ' termasuk ' +
+          suku +
+          ' salam dari medan'
+    );
+}
+
+function kampus(univ, jurusan) {
+    console.log(
+        'saya merupakan mahasiswa dari ' +
+         univ + 
+         ' mengambil jurusan' +
+         jurusan + 
+         ' sekian perkenalan saya'
+    );
+}
+
+hello('Elvina', 'Medan', 'Melayu');
+kampus('Polmed', 'telkom');5
